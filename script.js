422d6e51bcbd3106c3562b93c13012c441ef1fb5
tod619 <trevorodwyer619@hotmail.com>
2021-12-18 15:33:03 +0000

    create functions to show/hide modal in script.js file

---
+++ script.js
@@ -0,0 +1,25 @@
+const modalShow = document.getElementById('show-modal')
+const modal = document.getElementById('modal')
+const modalClose = document.getElementById('close-modal')
+const bookmarkForm = document.getElementById('bookmark-form')
+const websiteNameEl = document.getElementById('website-name')
+const wensiteUrlEl = document.getElementById('website-url')
+const bookmarksContainer = document.getElementById('bookmarks-container')
+
+// Functions
+// show modal function
+function showModal() {
+    modal.classList.add('show-modal')
+    websiteNameEl.focus()
+}
+
+// Close modal
+function closeModal() {
+    modal.classList.remove('show-modal')
+}
+
+
+// Event listners
+modalShow.addEventListener('click', showModal)
+
+modalClose.addEventListener('click', closeModal)
