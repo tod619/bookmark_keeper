4755ce22c596497342c05bb4ab60236ff2606e29
tod619 <trevorodwyer619@hotmail.com>
2022-01-04 20:00:08 +0000

    create storeBookmark function in script.js file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const modal = document.getElementById('modal')
 const modalClose = document.getElementById('close-modal')
 const bookmarkForm = document.getElementById('bookmark-form')
 const websiteNameEl = document.getElementById('website-name')
-const wensiteUrlEl = document.getElementById('website-url')
+const websiteUrlEl = document.getElementById('website-url')
 const bookmarksContainer = document.getElementById('bookmarks-container')
 
 // Functions
@@ -18,6 +18,22 @@ function closeModal() {
     modal.classList.remove('show-modal')
 }
 
+// handle data from form
+function storeBookmark(e) {
+    e.preventDefault()
+    const nameVaule = websiteNameEl.value
+    let urlValue = websiteUrlEl.value
+
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`; 
+    }
+    //console.log(nameVaule, urlValue)
+}
+
+// note mistake
+// if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+//      urlValue = `https://${urlValue}`; 
+// }
 
 // Event listners
 modalShow.addEventListener('click', showModal)
@@ -27,3 +43,6 @@ modalClose.addEventListener('click', closeModal)
 window.addEventListener('click', (e) => {
     e.target === modal ? closeModal() : false
 })
+
+// Bookmark event listner
+bookmarkForm.addEventListener('submit', storeBookmark)
