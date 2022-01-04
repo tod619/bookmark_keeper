3c2b5534e42747da2daaebc580fefbe7a6c839e2
tod619 <trevorodwyer619@hotmail.com>
2022-01-04 20:19:33 +0000

    add function to validate form in script.js file

--- script.js
+++ script.js
@@ -18,16 +18,38 @@ function closeModal() {
     modal.classList.remove('show-modal')
 }
 
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression)
+    if(!nameValue || !urlValue) {
+        alert('Please submit values for both fields')
+        return false
+    }
+    if(!urlValue.match(regex)){
+        alert('Please provide a valid web address')
+        return false
+    }
+
+    // Valid form
+    return true
+
+}
+
 // handle data from form
 function storeBookmark(e) {
     e.preventDefault()
-    const nameVaule = websiteNameEl.value
+    const nameValue = websiteNameEl.value
     let urlValue = websiteUrlEl.value
 
     if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
         urlValue = `https://${urlValue}`; 
     }
     //console.log(nameVaule, urlValue)
+    if(!validate(nameValue,urlValue)) {
+        return false
+    } 
+
 }
 
 // note mistake
